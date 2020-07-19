a5d8790c185d2acec412fe4cf8b18f6d585261bf
Nicholas Blott <blottn@tcd.ie>
2020-07-19 20:32:27 +0100

    Map deploy args to ansible-playbook

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ansible-playbook -v -i $DIR/inventory --become-method=sudo $DIR/main.yml
+
+ansible-playbook -v -i $DIR/inventory --become-method=sudo $DIR/main.yml "$@"
